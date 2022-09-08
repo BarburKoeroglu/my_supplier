ef278bc6fbaf90d628e9c1a175cfbc9c124d8cbe
BarburKoeroglu <b.koeroglu@proton.me>
2022-09-08 12:48:35 +0200

    update Exception/delete Exception

--- backend/src/main/java/org/capstone/my_supplier/exception/OrderNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package org.capstone.my_supplier.exception;
-
-public class OrderNotFoundException extends RuntimeException {
-
-    public OrderNotFoundException(String orderId) {
-        super("Die Bestellung mit der Bestell-Id " + orderId + " wurde nicht gefunden!");
-    }
-}
--- backend/src/main/java/org/capstone/my_supplier/exception/ProductNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package org.capstone.my_supplier.exception;
-
-public class ProductNotFoundException extends RuntimeException {
-    public ProductNotFoundException(String productId) {
-        super("Das Produkt mit der Artikelnummer " + productId + " wurde nicht gefunden!");
-    }
-}
